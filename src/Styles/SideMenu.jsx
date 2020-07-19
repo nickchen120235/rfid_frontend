2023e28d5fb46f5ffd5e45a8eeac67ab20a66022
Nickchen Nick <nickchen120235@gmail.com>
2020-07-19 12:03:03 +0800

    add style to component

---
+++ src/Styles/SideMenu.jsx
@@ -0,0 +1,11 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+export const drawerWidth = 120
+
+const styles = makeStyles(themes => ({
+  drawer: {
+    width: drawerWidth
+  },
+}))
+
+export default styles
