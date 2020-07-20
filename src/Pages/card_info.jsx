a649ce80a7a09aa807c33b0e0e3a58cde376eee7
Nickchen Nick <nickchen120235@gmail.com>
2020-07-20 10:21:37 +0800

    add table and use api

--- src/Pages/card_info.jsx
+++ src/Pages/card_info.jsx
@@ -1,14 +1,41 @@
-import React from 'react'
-import { Container } from '@material-ui/core'
+import React, { useState, useEffect } from 'react'
+import { TableContainer, Table, TableBody, TableCell, TableHead, TableRow, Paper } from '@material-ui/core'
 
 import styles from '../Styles/Pages'
 
 const CardInfo = () => {
+  const [tableContent, setTableContent] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:4000/api/getAll', {
+      mode: 'cors'
+    })
+      .then(res => res.json()).then(data => setTableContent(data))
+  })
+
   const classes = styles()
   return(
-    <Container className={classes.container}>
-      <h1>card_info</h1>
-    </Container>
+    <Paper className={classes.container}>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Card ID</TableCell>
+              <TableCell>Added Date</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {tableContent.map(row => 
+              <TableRow key={row.card}>
+                <TableCell>{row.name}</TableCell>
+                <TableCell>{row.card}</TableCell>
+                <TableCell>{Date()}</TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Paper>
   )
 }
 
